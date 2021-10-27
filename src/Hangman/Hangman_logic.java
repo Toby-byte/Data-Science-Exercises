98649f3fdb5d528db897f3b8314ba7bd639abaf2
Toby-byte <tobiasbogenielsen@gmail.com>
2021-10-27 11:50:43 +0200

    Forbedret if statement, der er meget kortere

--- src/Hangman/Hangman_logic.java
+++ src/Hangman/Hangman_logic.java
@@ -4,62 +4,159 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Hangman_logic {
+
+    static void hangmanDrawingsInitialize(int count) {
+        if (count==1) {
+            System.out.println(
+                    "  _____\n" +
+                            " | * * |\n" +
+                            " |  _  |\n" +
+                            "  \\___/\n" +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "     ");
+        }else if (count==2) {
+            System.out.println(
+                    "  _____\n" +
+                            " | * * |\n" +
+                            " |  _  |\n" +
+                            "  \\___/\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "\n" +
+                            "     ");
+        }else if (count==3) {
+            System.out.println(
+                    "  _____\n" +
+                            " | * * |\n" +
+                            " |  _  |\n" +
+                            "  \\___/\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "   /\n" +
+                            "  /    ");
+        }else if (count==4) {
+            System.out.println(
+                    "  _____\n" +
+                            " | * * |\n" +
+                            " |  _  |\n" +
+                            "  \\___/\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "    |\n" +
+                            "   / \\\n" +
+                            "  /   \\");
+        }else if (count==5) {
+            System.out.println(
+                    "  _____\n" +
+                            " | * * |\n" +
+                            " |  _  |\n" +
+                            "  \\___/\n" +
+                            "   /|\n" +
+                            "  / |\n" +
+                            "    |\n" +
+                            "   / \\\n" +
+                            "  /   \\");
+
+        }else if (count==6) {
+            System.out.println("  _____\n" +
+                    " | * * |\n" +
+                    " |  _  |\n" +
+                    "  \\___/\n" +
+                    "   /|\\\n" +
+                    "  / | \\\n" +
+                    "    |\n" +
+                    "   / \\\n" +
+                    "  /   \\");
+        } else if (count==7) {
+            System.out.println("  _____\n" +
+                    " | * * |\n" +
+                    " |  _  |\n" +
+                    "  \\___/\n" +
+                    "   /|\\\n" +
+                    "  / | \\\n" +
+                    "    |\n" +
+                    "   / \\\n" +
+                    "  /   \\");
+            System.out.println("Stickman is dead");
+        }
+    }
+
+    static void makeNameArrayList(String HangmanName,ArrayList<String> name ) {
+        for (int i = 0; i < HangmanName.length(); i++) {
+            char character = HangmanName.charAt(i);
+            String letter = String.valueOf(character);
+            name.add(letter);
+        }
+    }
 
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
             // først finder man sin fil ved at bruge "file klassen"
-            // https://behu.gitbook.io/java-first-semester/topics/06-algorithms-and-data-structure/reading-a-file
             File HangmanNames = new File("ressources/HangmanNames.txt");
             // derefter laver man en scanner som scanner i HangmanNames identifier
             Scanner scanner = new Scanner(HangmanNames);
             // så længe scanner har næste linje, så printer den
-            String HangmanName = null;
+            String hangmanName = null;
             while (scanner.hasNextLine()){
-                HangmanName = scanner.nextLine();
+                hangmanName = scanner.nextLine();
             }
+
             ArrayList<String> name = new ArrayList<>();
-            for (int i = 0; i < HangmanName.length(); i++) {
-                char character = HangmanName.charAt(i);
-                String letter = String.valueOf(character);
-                name.add(letter);
-            }
-            System.out.println(name);
+            // Take letters from hangmanName and puts them into the ArrayList with the name: name
+            makeNameArrayList(hangmanName,name);
+
+            // is here to check the word in arraylist is correct
+            System.out.println("The word is "+hangmanName.length()+" letters");
 
             ArrayList<String> blank = new ArrayList<>();
-            blank.add("_");
-            blank.add("_");
-            blank.add("_");
-            blank.add("_");
-            blank.add("_");
-            blank.add("_");
 
+            for (int i = 0; i < name.size(); i++) {
+                blank.add("_");
+            }
+            // Laver blank om til en string
+            System.out.println(blank.stream().map(Object::toString)
+                    .collect(Collectors.joining("")));
+
+            int count = 0;
             while (true) {
-                String userGuess = sc.nextLine();
-
-                if (userGuess.contains(name.get(0))) {
-                    blank.set(0, name.get(0));
-                    System.out.println(blank);
-                } else if (userGuess.contains(name.get(1))) {
-                    blank.set(1, name.get(1));
-                    System.out.println(blank);
-                } else if (userGuess.contains(name.get(2))) {
-                    blank.set(2, name.get(2));
-                    System.out.println(blank);
-                } else if (userGuess.contains(name.get(3))) {
-                    blank.set(3, name.get(3));
-                    System.out.println(blank);
-                } else if (userGuess.contains(name.get(4))) {
-                    blank.set(4, name.get(4));
-                    System.out.println(blank);
-                } else if (userGuess.contains(name.get(5))) {
-                    blank.set(5, name.get(5));
-                    System.out.println(blank);
-                } else {
-                    System.out.println("sorry but that's wrong");
+                if (!blank.contains("_")) {
+                    System.out.println("you won the game!");
                     return;
+                } else if (count==7) {
+                    System.out.println("you lost the game");
+                    return;
+                }
+                try {
+                    String userGuess = sc.nextLine();
+                    if (userGuess.contains(name.get(name.indexOf(userGuess)))) {
+                        blank.set(name.indexOf(userGuess), name.get(name.indexOf(userGuess)));
+                        /*Jeg ved ikke hvordan denne kode virker,
+                         men den laver en ArrayListe om til en String*/
+                        String blankToString = blank.stream().map(Object::toString)
+                                .collect(Collectors.joining(""));
+                        System.out.println(blankToString);
+                    }  else {
+                        System.out.println("sorry but that's wrong");
+                        count++;
+                        System.out.println("you have "+count+" mistakes, you can have 6 in total");
+
+                        hangmanDrawingsInitialize(count);
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("That is not correct");
+                    count++;
+                    System.out.println("you have "+count+" mistakes, you can have 6 in total");
+
+                    hangmanDrawingsInitialize(count);
                 }
             }
 
--- src/Hangman/test.java
+++ src/Hangman/test.java
@@ -1,9 +1,32 @@
 package Hangman;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class test {
     public static void main(String[] args) {
-        String  name = null;
-        name = "Tobias";
-        System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> name = new ArrayList<>();
+        name.add("T");
+        name.add("o");
+        name.add("b");
+        name.add("i");
+        name.add("a");
+        name.add("s");
+
+        String userGuess = sc.nextLine();
+
+        String newStringName = name.stream().map(Object::toString)
+                .collect(Collectors.joining(""));
+
+        try {
+            if (userGuess.contains(name.get(name.indexOf(userGuess)))) {
+                System.out.println(userGuess + " is in there and the index is " + name.indexOf(userGuess));
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(userGuess + " is not in there");
+        }
+
     }
 }
